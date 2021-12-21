395e1e51e9cf008e64e56d9fd2cb72176d7c1c5f
AndX2 <andrew.android@yandex.ru>
2021-12-21 03:16:37 +0300

    AC-704 Онбординг

---
+++ lib/res/strings.dart
@@ -0,0 +1,20 @@
+// TODO: временный файл с константами литералов до реализации локализации https://acits.atlassian.net/browse/AC-796
+abstract class StringRes {
+  static const String commonNext = 'Далее',
+      commonBegin = 'Начать',
+      commonAppName = 'Acits';
+
+  static const String onboardingNewsTitle =
+          'Единая медицинская карта животного',
+      onboardingNewsMsg =
+          'Карта ведется на протяжении всей жизни животного и не зависит от приюта',
+      onboardingPlanTitle = 'Сводка назначений на текущий день',
+      onboardingPlanMsg =
+          'Позволяет четко планировать рабочее время и своевременно оказывать медицинскую помощь животным',
+      onboardingFreeTitle = 'Абсолютно бесплатно',
+      onboardingFreeMsg =
+          'Весь функционал будет доступен для Вас в полном объеме, без ограничений',
+      onboardingDrugsTitle = 'Гибкая система работы с назначениями',
+      onboardingDrugsMsg =
+          'Мы заранее подготовили максимальное количество медицинских назначений для приютов';
+}
---
+++ lib/screen/onboarding/onboarding_route.dart
@@ -0,0 +1,7 @@
+import 'package:acits_flutter/screen/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+/// Роут экрана онбординга
+class OnboardingRoute extends MaterialPageRoute {
+  OnboardingRoute() : super(builder: (_) => const OnboardingScreen());
+}
