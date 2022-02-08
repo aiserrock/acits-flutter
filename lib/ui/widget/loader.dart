b12979f446d3e11f241f5c0dfa00b792865db14d
aiserrock <aicserrock@gmail.com>
2022-02-08 18:56:03 +0300

    ac-866 add edit curator

---
+++ lib/ui/widget/loader.dart
@@ -0,0 +1,30 @@
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+const _sizePart = .75;
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({
+    this.assetPath,
+    Key? key,
+  }) : super(key: key);
+
+  final String? assetPath;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (_, cons) {
+      final size = min(cons.maxHeight, cons.maxWidth) * _sizePart;
+      return Center(
+        child: Lottie.asset(
+          assetPath ?? 'assets/lottie/loading.json',
+          height: size,
+          width: size,
+        ),
+      );
+    });
+  }
+}
