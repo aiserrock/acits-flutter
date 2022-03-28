f9bd982049f8c1c1b19a3f9120cb37d4e2c4c64c
aiserrock <aicserrock@gmail.com>
2022-03-28 08:12:45 +0300

    29g7988-ci-cd-ios

--- lib/generated/intl/messages_ru.dart
+++ lib/generated/intl/messages_ru.dart
@@ -32,52 +32,73 @@ class MessageLookup extends MessageLookupByLibrary {
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "animalAdd": MessageLookupByLibrary.simpleMessage("Новое животное"),
-        "animalAdditionalInfo": MessageLookupByLibrary.simpleMessage("Дополнительная информация"),
+        "animalAdditionalInfo":
+            MessageLookupByLibrary.simpleMessage("Дополнительная информация"),
         "animalAdmitted": MessageLookupByLibrary.simpleMessage("Поступил"),
         "animalAge": MessageLookupByLibrary.simpleMessage("Возраст"),
         "animalAgeAppox": MessageLookupByLibrary.simpleMessage("Примерный"),
         "animalAgeExact": MessageLookupByLibrary.simpleMessage("Точный"),
-        "animalAnimalFamily": MessageLookupByLibrary.simpleMessage("Семейство животного"),
-        "animalAnimalKind": MessageLookupByLibrary.simpleMessage("Вид животного"),
-        "animalAnimalStatus": MessageLookupByLibrary.simpleMessage("Статус животного"),
+        "animalAnimalFamily":
+            MessageLookupByLibrary.simpleMessage("Семейство животного"),
+        "animalAnimalKind":
+            MessageLookupByLibrary.simpleMessage("Вид животного"),
+        "animalAnimalStatus":
+            MessageLookupByLibrary.simpleMessage("Статус животного"),
         "animalApplicant": MessageLookupByLibrary.simpleMessage("Заявитель"),
         "animalBirth": MessageLookupByLibrary.simpleMessage("Дата рождения"),
-        "animalCardTitle": MessageLookupByLibrary.simpleMessage("Карточка животного"),
-        "animalCatchPlace": MessageLookupByLibrary.simpleMessage("Место отлова"),
+        "animalCardTitle":
+            MessageLookupByLibrary.simpleMessage("Карточка животного"),
+        "animalCatchPlace":
+            MessageLookupByLibrary.simpleMessage("Место отлова"),
         "animalCategory": MessageLookupByLibrary.simpleMessage("Категория"),
         "animalChip": MessageLookupByLibrary.simpleMessage("Кольцо/чип"),
-        "animalChipDate": MessageLookupByLibrary.simpleMessage("Дата чипирования"),
+        "animalChipDate":
+            MessageLookupByLibrary.simpleMessage("Дата чипирования"),
         "animalColor": MessageLookupByLibrary.simpleMessage("Окрас"),
         "animalComments": MessageLookupByLibrary.simpleMessage("Комментарии"),
-        "animalCommonInfo": MessageLookupByLibrary.simpleMessage("Основная информация"),
+        "animalCommonInfo":
+            MessageLookupByLibrary.simpleMessage("Основная информация"),
         "animalCurator": MessageLookupByLibrary.simpleMessage("Куратор"),
         "animalCuratorAddress": MessageLookupByLibrary.simpleMessage("Адрес"),
         "animalCuratorEmail": MessageLookupByLibrary.simpleMessage("E-mail"),
-        "animalCuratorLastName": MessageLookupByLibrary.simpleMessage("Фамилия"),
+        "animalCuratorLastName":
+            MessageLookupByLibrary.simpleMessage("Фамилия"),
         "animalCuratorName": MessageLookupByLibrary.simpleMessage("Имя"),
-        "animalCuratorPhone": MessageLookupByLibrary.simpleMessage("Номер телефона"),
-        "animalDateAdmitt": MessageLookupByLibrary.simpleMessage("Дата поступления"),
+        "animalCuratorPhone":
+            MessageLookupByLibrary.simpleMessage("Номер телефона"),
+        "animalDateAdmitt":
+            MessageLookupByLibrary.simpleMessage("Дата поступления"),
         "animalEdit": MessageLookupByLibrary.simpleMessage("Редактировать"),
         "animalFamily": MessageLookupByLibrary.simpleMessage("Семейство"),
         "animalGenderFemale": MessageLookupByLibrary.simpleMessage("Женский"),
         "animalGenderLess": MessageLookupByLibrary.simpleMessage("Без пола"),
         "animalGenderMale": MessageLookupByLibrary.simpleMessage("Мужской"),
         "animalGenderMiddle": MessageLookupByLibrary.simpleMessage("Средний"),
-        "animalGenderUndefined": MessageLookupByLibrary.simpleMessage("Неизвестен"),
+        "animalGenderUndefined":
+            MessageLookupByLibrary.simpleMessage("Неизвестен"),
         "animalKind": MessageLookupByLibrary.simpleMessage("Вид"),
         "animalName": MessageLookupByLibrary.simpleMessage("Кличка"),
-        "animalPickCurator": MessageLookupByLibrary.simpleMessage("Выбрать куратора"),
-        "animalPrescriptions": MessageLookupByLibrary.simpleMessage("Назначения"),
-        "animalQtyMonth": MessageLookupByLibrary.simpleMessage("Кол-во месяцев"),
+        "animalPickCurator":
+            MessageLookupByLibrary.simpleMessage("Выбрать куратора"),
+        "animalPrescriptions":
+            MessageLookupByLibrary.simpleMessage("Назначения"),
+        "animalQtyMonth":
+            MessageLookupByLibrary.simpleMessage("Кол-во месяцев"),
         "animalQtyYear": MessageLookupByLibrary.simpleMessage("Кол-во лет"),
-        "animalReceiptDate": MessageLookupByLibrary.simpleMessage("Дата поступления"),
+        "animalReceiptDate":
+            MessageLookupByLibrary.simpleMessage("Дата поступления"),
         "animalSex": MessageLookupByLibrary.simpleMessage("Пол"),
-        "animalSocialLink": MessageLookupByLibrary.simpleMessage("Ссылка на соцсети"),
-        "animalSpecSigns": MessageLookupByLibrary.simpleMessage("Особые приметы"),
+        "animalSocialLink":
+            MessageLookupByLibrary.simpleMessage("Ссылка на соцсети"),
+        "animalSpecSigns":
+            MessageLookupByLibrary.simpleMessage("Особые приметы"),
         "animalStatus": MessageLookupByLibrary.simpleMessage("Статус"),
-        "animalStatusAndJoin": MessageLookupByLibrary.simpleMessage("Статус и поступление"),
-        "animalTransferAct": MessageLookupByLibrary.simpleMessage("Акт приема-передачи"),
-        "animalUploadAct": MessageLookupByLibrary.simpleMessage("Загрузить акт приема-передачи"),
+        "animalStatusAndJoin":
+            MessageLookupByLibrary.simpleMessage("Статус и поступление"),
+        "animalTransferAct":
+            MessageLookupByLibrary.simpleMessage("Акт приема-передачи"),
+        "animalUploadAct": MessageLookupByLibrary.simpleMessage(
+            "Загрузить акт приема-передачи"),
         "animalWeight": MessageLookupByLibrary.simpleMessage("Вес, кг"),
         "aninmalSize": MessageLookupByLibrary.simpleMessage("Рост, см"),
         "applicantAdd": MessageLookupByLibrary.simpleMessage("Новый заявитель"),
@@ -95,13 +116,15 @@ class MessageLookup extends MessageLookupByLibrary {
         "commonEdit": MessageLookupByLibrary.simpleMessage("Редактировать"),
         "commonErrorStubMsg": MessageLookupByLibrary.simpleMessage(
             "Не удалось загрузить данные.\nПожалуйста, попробуйте еще раз."),
-        "commonErrorStubTitle": MessageLookupByLibrary.simpleMessage("Ой, что-то пошло не так..."),
+        "commonErrorStubTitle":
+            MessageLookupByLibrary.simpleMessage("Ой, что-то пошло не так..."),
         "commonMonth": MessageLookupByLibrary.simpleMessage("месяц"),
         "commonNDays": m0,
         "commonNMonth": m1,
         "commonNYears": m2,
         "commonNext": MessageLookupByLibrary.simpleMessage("Далее"),
-        "commonNotCompleted": MessageLookupByLibrary.simpleMessage("Не выполнено"),
+        "commonNotCompleted":
+            MessageLookupByLibrary.simpleMessage("Не выполнено"),
         "commonReloadBtn": MessageLookupByLibrary.simpleMessage("Обновить"),
         "commonReschedule": MessageLookupByLibrary.simpleMessage("Перенести"),
         "commonSearch": MessageLookupByLibrary.simpleMessage("Поиск"),
@@ -110,36 +133,46 @@ class MessageLookup extends MessageLookupByLibrary {
         "commonYear": MessageLookupByLibrary.simpleMessage("год"),
         "curatorAdd": MessageLookupByLibrary.simpleMessage("Новый куратор"),
         "curatorEdit": MessageLookupByLibrary.simpleMessage("Редактирование"),
-        "loginAuthorizeError": MessageLookupByLibrary.simpleMessage("Неверный логин или пароль"),
+        "loginAuthorizeError":
+            MessageLookupByLibrary.simpleMessage("Неверный логин или пароль"),
         "loginDescribeMsg": MessageLookupByLibrary.simpleMessage(
             "ACITS – Система контроля за животными\nвнутри приюта"),
         "loginEntryBtn": MessageLookupByLibrary.simpleMessage("Войти"),
-        "loginForgetPass": MessageLookupByLibrary.simpleMessage("Забыли пароль?"),
-        "loginLoginHint": MessageLookupByLibrary.simpleMessage("Введите почту или логин"),
+        "loginForgetPass":
+            MessageLookupByLibrary.simpleMessage("Забыли пароль?"),
+        "loginLoginHint":
+            MessageLookupByLibrary.simpleMessage("Введите почту или логин"),
         "loginLoginLabel": MessageLookupByLibrary.simpleMessage("Логин"),
         "loginPassLabel": MessageLookupByLibrary.simpleMessage("Пароль"),
-        "loginToRegistration": MessageLookupByLibrary.simpleMessage("Зарегистрироваться"),
-        "mainAddAppointments": MessageLookupByLibrary.simpleMessage("Добавить назначение"),
+        "loginToRegistration":
+            MessageLookupByLibrary.simpleMessage("Зарегистрироваться"),
+        "mainAddAppointments":
+            MessageLookupByLibrary.simpleMessage("Добавить назначение"),
         "mainAnimal": MessageLookupByLibrary.simpleMessage("Животное"),
         "mainAppoinment": MessageLookupByLibrary.simpleMessage("Назначение"),
-        "mainAppoinmentAuthor": MessageLookupByLibrary.simpleMessage("Автор назначения"),
+        "mainAppoinmentAuthor":
+            MessageLookupByLibrary.simpleMessage("Автор назначения"),
         "mainAppointments": MessageLookupByLibrary.simpleMessage("Назначения"),
-        "mainEmptyState": MessageLookupByLibrary.simpleMessage("На сегодня назначений нет"),
+        "mainEmptyState":
+            MessageLookupByLibrary.simpleMessage("На сегодня назначений нет"),
         "mainTitle": MessageLookupByLibrary.simpleMessage("Сегодня"),
         "onboardingDrugsMsg": MessageLookupByLibrary.simpleMessage(
             "Мы заранее подготовили максимальное количество медицинских назначений для приютов"),
-        "onboardingDrugsTitle":
-            MessageLookupByLibrary.simpleMessage("Гибкая система работы с назначениями"),
+        "onboardingDrugsTitle": MessageLookupByLibrary.simpleMessage(
+            "Гибкая система работы с назначениями"),
         "onboardingFreeMsg": MessageLookupByLibrary.simpleMessage(
             "Весь функционал будет доступен для Вас в полном объеме, без ограничений"),
-        "onboardingFreeTitle": MessageLookupByLibrary.simpleMessage("Абсолютно бесплатно"),
+        "onboardingFreeTitle":
+            MessageLookupByLibrary.simpleMessage("Абсолютно бесплатно"),
         "onboardingNewsMsg": MessageLookupByLibrary.simpleMessage(
             "Карта ведется на протяжении всей жизни животного и не зависит от приюта"),
-        "onboardingNewsTitle": MessageLookupByLibrary.simpleMessage("Единая карта животного"),
+        "onboardingNewsTitle":
+            MessageLookupByLibrary.simpleMessage("Единая карта животного"),
         "onboardingPlanMsg": MessageLookupByLibrary.simpleMessage(
             "Позволяет четко планировать рабочее время и своевременно оказывать медицинскую помощь животным"),
-        "onboardingPlanTitle":
-            MessageLookupByLibrary.simpleMessage("Сводка назначений на текущий день"),
-        "shelterSelectShelter": MessageLookupByLibrary.simpleMessage("Выберите приют")
+        "onboardingPlanTitle": MessageLookupByLibrary.simpleMessage(
+            "Сводка назначений на текущий день"),
+        "shelterSelectShelter":
+            MessageLookupByLibrary.simpleMessage("Выберите приют")
       };
 }
