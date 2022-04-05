8176bbcb4a89234c96cfe7ac7ed54b533a51323e
AndX2 <andrew.android@yandex.ru>
2022-04-05 09:43:03 +0300

    2a74hc8 debug ssl overrides

--- test/dev/main.dart
+++ test/dev/main.dart
@@ -1,13 +1,18 @@
+import 'dart:io';
+
 import 'package:acits_flutter/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
 import 'di/di_container.dart';
+import 'res/cert_res.dart';
+import 'ssl/http_overrides.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome?.setPreferredOrientations([DeviceOrientation.portraitUp]);
   _setupLogging();
+  _addDebugHttpCerts();
   await initDevDi();
   runApp(const MyApp());
 }
@@ -18,4 +23,16 @@ void _setupLogging() {
     // ignore: avoid_print
     print('${rec.level.name}: ${rec.time}: ${rec.message}');
   });
+}
+
+void _addDebugHttpCerts() {
+  HttpOverrides.global = SslHttpOverrides(
+    //ignore: avoid_redundant_argument_values
+    withTrustedRoots: true,
+    certFilePathes: [
+      CertRes.nikitaAir13,
+      CertRes.nikitaMac2013,
+      CertRes.nikitaMacPro13,
+    ],
+  );
 }
